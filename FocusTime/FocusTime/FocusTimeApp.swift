e178d1d00bd351494ad0cb885c47403ce30b9041
Artem Kudryavtsev <himynameisartem@icloud.com>
2025-08-09 11:44:06 +0300

    initial commit

---
+++ FocusTime/FocusTime/FocusTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimeApp.swift
+//  FocusTime
+//
+//  Created by Artem Kudryavtsev on 09.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
